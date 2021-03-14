3eb53e96ace4609698a79dfc98e6c2d244ee1d96
Vyse12138 <zhuyulei12333@gmail.com>
2021-03-14 14:12:57 +1100

    loading and not found page update

---
+++ src/components/DrinkList/DrinksListEmpty.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="empty">
+    <div class="drink">
+    <div class="drink-text">
+      <h4 class="drink-text-title">Oops, can't find anything </h4>
+      <p class="drink-text-desc"> :(</p>
+    </div>
+    <img
+      class="drink-img"
+      src="../../assets/not-found.png"
+      alt="nothing found"
+    />
+    </div>
+
+  </div>
+</template>
+
+<style scoped>
+.empty {
+  margin: 10px auto;
+  width: 700px;
+}
+.drink {
+  padding: 1.5rem 2rem;
+  height: 100px;
+  display: flex;
+  background-color: #ffffff;
+}
+.drink-text {
+  text-align: left;
+  margin-top: -0.75em;
+  flex: 1 1 auto;
+  padding-right: 1rem;
+}
+.drink-img {
+  flex: 0 0 auto;
+  width: 100px;
+  height: 100px;
+}
+</style>
---
+++ src/components/DrinkList/DrinksListLoading.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="loading">
+    <div class="drink" v-for="item in items" :key="item">
+      <transition-group name="breath">
+        <div v-if="show" class="drink-text">
+          <h4 class="drink-text-title" />
+          <p class="drink-text-desc" />
+        </div>
+        <div v-if="show" class="drink-img" />
+      </transition-group>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, onUnmounted, onMounted } from "vue";
+
+export default defineComponent({
+  setup() {
+    const show = ref(true);
+
+    let loadingBreathEffect = setInterval(() => {
+      show.value = !show.value;
+      console.log(show.value);
+    }, 1000);
+
+    onUnmounted(() => {
+      clearInterval(loadingBreathEffect);
+    });
+    return { show, items: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] };
+  }
+});
+</script>
+
+<style scoped>
+.loading {
+  margin: 10px auto;
+  width: 700px;
+}
+.drink {
+  padding: 1.5rem 2rem;
+  height: 100px;
+  display: flex;
+  border-bottom: 1px solid rgba(178, 186, 194, 0.15);
+  background-color: #ffffff;
+}
+.drink-text {
+  text-align: left;
+  margin-top: -0.75em;
+  flex: 1 1 auto;
+  padding-right: 1rem;
+}
+.drink-text-title {
+  background-color: #f4f5f5;
+  width: 15rem;
+  height: 1.5rem;
+}
+.drink-text-desc {
+  background-color: #f4f5f5;
+  width: 30rem;
+  height: 3rem;
+}
+
+.drink-img {
+  background-color: #f4f5f5;
+  flex: 0 0 auto;
+  width: 100px;
+  height: 100px;
+}
+
+.breath-enter-active {
+  transition: opacity 1s ease;
+}
+.breath-leave-active {
+  transition: opacity 1s ease;
+}
+.breath-enter-from,
+.breath-leave-to {
+  opacity: 0.5;
+}
+</style>
